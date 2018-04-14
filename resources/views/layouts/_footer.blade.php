78934e043809508229ecb7ed1c6b12e2b4f2dad1
Jehaz <jehaz.xu@outlook.com>
2018-04-14 09:17:21 +0000

    adjust style

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="#" target="_blank">博飞科技 软件开发部门</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="#">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:jehaz.xu@gmail.com">联系我们</a></p>
     </div>
 </footer>
