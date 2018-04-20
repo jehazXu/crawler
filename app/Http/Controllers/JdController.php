53071fb45e123facfd41650e9d601f8618e62138
vagrant <vagrant@homestead>
2018-04-20 07:07:27 +0000

    modify

--- app/Http/Controllers/JdController.php
+++ app/Http/Controllers/JdController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Redis;
 use QL\QueryList;
 
 use Illuminate\Http\Request;
@@ -11,15 +12,66 @@ class JdController extends Controller
     }
 
     function crawler(Request $request){
-        $res[0]=0;
-        $page=$request->page-1;
         $sortType=$request->sorttype;
-        $url="https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv474&productId=".$request->pid."&score=0&sortType=".$sortType."&page=".$page."&pageSize=10&isShadowSku=0&rid=0&fold=1";
+        $pid=$request->pid;
+        $nickname=$request->nickname;
+        dump($request->all());exit;
+        $array=array();
+        $arraydata=self::getAllComments($pid,$sortType,1,$array);
+        $data=self::searchInComments($arraydata,'nickname',$request->nickname);
+        return json_encode($data);
+    }
+
+    static function getAllComments($pid,$type,$page,&$array)
+    {
+        $arraydata=self::getIndexComments($pid,$type,$page);
+        if(count($arraydata)>=10){
+            $page++;
+            // 防止浏览器报429 too many request错误，设置每取20条数据暂停5秒
+            if($page%20==0)
+            {
+                sleep(5);
+            }
+            //递归下一页
+            self::getAllComments($pid,$type,$page,$array);
+        }
+        if(count($array) && count($arraydata)){
+            $array=array_merge($arraydata,$array);
+        }
+        else{
+            $array=$arraydata;
+        }
+        return $array;
+    }
+
+    static function getIndexComments($pid,$type,$page)
+    {
+        // score表示评论的类型（好评为3 中评为2 差评为1 全部评论为0 追评为5）
+        // pageSize是每页最多的评论数（最大为10）
+        $url="https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv474&productId=".$pid."&score=0&sortType=".$type."&page=".$page."&pageSize=10&isShadowSku=0&rid=0&fold=0";
         $cnt = file_get_contents($url);
         $content= mb_convert_encoding($cnt ,"UTF-8","GBK");
         $data=mb_substr($content,25,count($content)-3,'UTF-8');
-        $jsondata=json_decode($data, true);
-        $comments=$jsondata['comments'];
-        return json_encode($comments);
+        $arraydata=json_decode($data, true);
+        $array=$arraydata['comments'];
+        return $array;
     }
+
+    static function searchInComments($array,$key,$value)
+    {
+        $res=array();
+        dump($value.'+');
+        $va=substr($value, 0,1).'***'.substr($value,strlen($value)-1,1);
+        dump($va.'~');
+        foreach($array as $keyp=>$valuep){
+            var_dump($valuep[$key]);echo '--';
+            if($valuep[$key]==$va){
+                $re[$key]=$valuep[$key];
+                $re['creationTime']=$valuep['creationTime'];
+                array_push($res, $re);
+            }
+        }
+        return $res;
+    }
+
 }
--- resources/views/jds/show.blade.php
+++ resources/views/jds/show.blade.php
@@ -6,20 +6,20 @@
     <link rel="stylesheet" href="{{asset('libs/bootstrap-table-master/dist/bootstrap-table.min.css')}}">
     <div class="form-horizontal" action="{{route('api.jd.crawler')}}" method="post">
       <div class="form-group">
-        <label for="inputEmail3" class="col-sm-2 control-label">京东产品ID</label>
+        <label class="col-sm-2 control-label">京东产品ID</label>
         <div class="col-sm-10">
           <input type="number" class="form-control" id="pid" name="pid" placeholder="输入京东产品ID" value="10561776205">
           <span style="color: #88888888">示例：网址蓝色数字</span><img style="margin-top: 5px;opacity: 0.68;" src="{{asset('images/jd_url_pid.png')}}">
         </div>
       </div>
       <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">页码</label>
+        <label class="col-sm-2 control-label">用户名</label>
         <div class="col-sm-10">
-          <input type="number" class="form-control" id="page" name="page" placeholder="页码" value="1">
+          <input type="text" class="form-control" id="nickname" name="nickname" placeholder="用户完整名" />
         </div>
       </div>
       <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">评论排序规则</label>
+        <label class="col-sm-2 control-label">评论排序规则</label>
         <div class="col-sm-10">
           <input id="r1" type="radio" value='5' name="sorttype" checked="true" >京东默认推荐排序</input>
           <input id="r2" type="radio" value='6' name="sorttype">时间排序</input>
@@ -27,7 +27,7 @@
       </div>
       <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-          <button type="submit" id="sub" class="btn btn-success"><span class="glyphicon glyphicon-magnet" aria-hidden="true"></span> 抓取评论数据</button>
+          <button type="submit" id="sub" class="btn btn-success"><span class="glyphicon glyphicon-magnet" aria-hidden="true"></span> 从评论中搜索</button>
         </div>
       </div>
     </div>
@@ -60,8 +60,8 @@
     <script src="{{asset('libs/bootstrap-table-master/dist/locale/bootstrap-table-zh-CN.min.js')}}"></script>
     <script src="{{asset('libs/loading-master/js/loading.js')}}"></script>
     <script type="text/javascript">
+        var add=0;
         $('#sub').click(function(){
-
             $('body').loading({
                 loadingWidth:240,
                 title:'获取中...请稍等!',
@@ -78,7 +78,10 @@
                 loadingBg:'rgba(20,125,148,0.8)',
                 loadingMaskBg:'rgba(123,122,222,0.2)'
             });
-            $.post('{{route('api.jd.crawler')}}',{'pid':$('#pid').val(),'page':$('#page').val(),'sorttype':$("input[name='sorttype']:checked").val()},function(data){
+            if(add==0){
+                $('#comment-list').html('');
+            }
+            $.post('{{route('api.jd.crawler')}}',{'pid':$('#pid').val(),'sorttype':$("input[name='sorttype']:checked").val()},function(data){
                 var obj = JSON.parse(data);
                 if(obj){
                     removeLoading('loadfram');
